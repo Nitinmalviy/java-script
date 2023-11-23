dc1762622d22c0c40ce244f49f561901de646c46
Nitin Malviya <94914379+Nitinmalviy@users.noreply.github.com>
2023-11-23 17:03:55 +0530

    Uplode
    By Nitin js pracice

---
+++ string.js
@@ -0,0 +1,8 @@
+// var name = new String("Nitin");
+// var name2 = new  String("Muskan");
+var name = "NItin";
+var surname = "Malviya";
+// console.log(a.length);
+console.log("Ji".replace(name));
+
+
